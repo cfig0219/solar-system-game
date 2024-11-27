200cc08e865b1f9ec0443e7717a763a1c679af32
cfig0219 <100231908+cfig0219@users.noreply.github.com>
2024-11-27 07:44:08 -0500

    Add files via upload

---
+++ Rocket.js
@@ -0,0 +1,55 @@
+export class Rocket {
+    /**
+     * Initializes the Base class
+     * @param {BABYLON.Vector3} rocketLocation - Position in 3D space
+     * @param {BABYLON.Scene} scene - The Babylon scene
+     * @param {BABYLON.camera} camera - The camera to adjust render distance
+     */
+    constructor(rocketLocation, scene, camera) {
+        this.rocketSize = 20;
+        this.rocketLocation = rocketLocation;
+        this.rocketMass = 20;
+        this.rocketTexture = new BABYLON.Texture('Textures/Base/borg.jpg', scene);
+        this.scene = scene;
+
+        // Create a cube (box)
+        this.cube = BABYLON.MeshBuilder.CreateBox("cube", { size: this.rocketSize * 2 }, this.scene);
+        
+        // Create a gray material
+        const cubeMaterial = new BABYLON.StandardMaterial("cubeMaterial", this.scene);
+        cubeMaterial.diffuseTexture = this.rocketTexture;
+        cubeMaterial.specularColor = new BABYLON.Color3(0, 0, 0); // Remove highlights
+        cubeMaterial.emissiveColor = new BABYLON.Color3(0, 0, 0); // No self-illumination
+        
+        // Apply material to cube
+        this.cube.material = cubeMaterial;
+
+        // Set cube position
+        this.cube.position = this.rocketLocation;
+        this.cube.renderingGroupId = 1;
+    }
+	
+    
+    // Physics related functions
+    setMass(newMass) {
+		this.mass = newMass;
+    }
+    
+    getMass() {
+    	return this.rocketMass;
+    }
+    
+    getLocation() {
+    	return this.rocketLocation;
+    }
+    
+    setLocation(newLocation) {
+        this.rocketLocation = newLocation;
+        this.cube.position = this.rocketLocation;
+    }
+    
+    setRotation(yRotation) {
+        this.rotation = yRotation;
+        this.cube.rotation.y = yRotation;
+    }
+}
