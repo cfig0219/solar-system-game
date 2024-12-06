f0557a81b55fd51ef6f9e89b5cab7df610dab39f
cfig0219 <100231908+cfig0219@users.noreply.github.com>
2024-12-06 09:37:30 -0500

    Add files via upload

--- Player.js
+++ Player.js
@@ -2,6 +2,7 @@
 import { Rocket } from './Rocket.js';
 import { Display } from './Display.js';
 import { Resources } from './Resources.js';
+import { Buttons } from './Buttons.js';
 
 export class Player {
     /**
@@ -25,7 +26,7 @@ export class Player {
         this.accelerationFactor = 50; // Scale of acceleration
         
         // Calls function to initialize player controls
-        this.setButtons();
+        this.buttons = new Buttons(scene, camera);
         this.setControls();
         
         // Creates rocket class object
@@ -46,129 +47,6 @@ export class Player {
         this.display.displayTier(this.techTier);
     }
     
-    
-    // Initializes buttons
-    setButtons() {
-        // Left side buttons
-        const buttonContainer = document.createElement("div");
-        buttonContainer.style.position = "absolute";
-        buttonContainer.style.bottom = "20%";
-        buttonContainer.style.left = "10%";
-        buttonContainer.style.display = "flex";
-        buttonContainer.style.flexDirection = "column";
-        document.body.appendChild(buttonContainer);
-        
-        // X and Y offsets for positioning buttons
-        const buttonOffsets = [
-            { x: 0, y: 0 },  // Top button
-            { x: 0, y: 80 },  // Bottom button
-            { x: -70, y: -50 }, // Left button
-            { x: 70, y: -110 },  // Right button
-        ];
-        
-        // Button displays
-        const buttonTexts = ["^", "v", "<", ">"];
-    
-        for (let i = 0; i < 4; i++) {
-            const button = document.createElement("button");
-            button.innerText = buttonTexts[i];
-            button.style.width = "60px";
-            button.style.height = "60px";
-            button.style.backgroundColor = "#008000";
-            button.style.transform = `translate(${buttonOffsets[i].x}px, ${buttonOffsets[i].y}px)`;
-            button.style.color = "white";
-            button.style.border = "none";
-            button.style.borderRadius = "5px";
-            button.style.cursor = "pointer";
-            button.style.fontFamily = "Arial, sans-serif";
-            button.style.fontSize = "14px";
-            button.style.zIndex = "1";
-            buttonContainer.appendChild(button);
-        }
-        
-        // Right side buttons
-        const buttonContainer2 = document.createElement("div");
-        buttonContainer2.style.position = "absolute";
-        buttonContainer2.style.bottom = "25%";
-        buttonContainer2.style.right = "5%";
-        buttonContainer2.style.display = "flex";
-        buttonContainer2.style.gap = "10px";
-        buttonContainer2.style.flexDirection = "column";
-        document.body.appendChild(buttonContainer2);
-        
-        const buttonTexts2 = ["^", "rcs", "v"];
-        
-        for (let i = 0; i < 3; i++) {
-            const button = document.createElement("button");
-            button.innerText = buttonTexts2[i];
-            button.style.width = "60px";
-            button.style.height = "60px";
-            button.style.backgroundColor = "#008000";
-            button.style.color = "white";
-            button.style.border = "none";
-            button.style.borderRadius = "5px";
-            button.style.cursor = "pointer";
-            button.style.fontFamily = "Arial, sans-serif";
-            button.style.fontSize = "14px";
-            button.style.zIndex = "1";
-            buttonContainer2.appendChild(button);
-        }
-        
-        // Bottom buttons
-        const buttonContainer3 = document.createElement("div");
-        buttonContainer3.style.position = "absolute";
-        buttonContainer3.style.bottom = "5%";
-        buttonContainer3.style.left = "35%";
-        buttonContainer3.style.display = "flex";
-        buttonContainer3.style.gap = "30px";
-        document.body.appendChild(buttonContainer3);
-        
-        const buttonTexts3 = ["drill", "warp"];
-        
-        for (let i = 0; i < 2; i++) {
-            const button = document.createElement("button");
-            button.innerText = buttonTexts3[i];
-            button.style.width = "200px";
-            button.style.height = "60px";
-            button.style.backgroundColor = "#008000";
-            button.style.color = "white";
-            button.style.border = "none";
-            button.style.borderRadius = "5px";
-            button.style.cursor = "pointer";
-            button.style.fontFamily = "Arial, sans-serif";
-            button.style.fontSize = "14px";
-            button.style.zIndex = "1";
-            buttonContainer3.appendChild(button);
-        }
-        
-        // Top buttons
-        const buttonContainer4 = document.createElement("div");
-        buttonContainer4.style.position = "absolute";
-        buttonContainer4.style.top = "5%";
-        buttonContainer4.style.left = "35%";
-        buttonContainer4.style.display = "flex";
-        buttonContainer4.style.gap = "30px";
-        document.body.appendChild(buttonContainer4);
-        
-        const buttonTexts4 = ["upgrade", "sell ore"];
-        
-        for (let i = 0; i < 2; i++) {
-            const button = document.createElement("button");
-            button.innerText = buttonTexts4[i];
-            button.style.width = "200px";
-            button.style.height = "60px";
-            button.style.backgroundColor = "#008000";
-            button.style.color = "white";
-            button.style.border = "none";
-            button.style.borderRadius = "5px";
-            button.style.cursor = "pointer";
-            button.style.fontFamily = "Arial, sans-serif";
-            button.style.fontSize = "14px";
-            button.style.zIndex = "1";
-            buttonContainer4.appendChild(button);
-        }
-    }
-
     
     // Sets the keyboard and button controls to move player
     setControls() {        
